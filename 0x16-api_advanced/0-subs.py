5c2f16578a0d311b9de52c481cc31c1a17495f4e
KingBygone0 <kingbygone@icloud.com>
2024-03-05 12:06:07 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers """
-
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to return the number of subscribers"""
-
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0)\
-    Gecko/20100101 Firefox/72.0'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    else:
-        return response.json().get('data').get('subscribers')
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
